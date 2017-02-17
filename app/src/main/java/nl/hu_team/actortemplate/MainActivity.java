cbf4b73241c55d67149677037732313e7aae9369
Arjan <arjan.shojaeian@gmail.com>
2017-02-17 20:32:24 +0100

    Implemented butterknife library

--- app/src/main/java/nl/hu_team/actortemplate/MainActivity.java
+++ app/src/main/java/nl/hu_team/actortemplate/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
