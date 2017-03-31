1a9c9b56caa83ed487384bfb1f89ca7ea925f534
Arjan <arjan.shojaeian@gmail.com>
2017-03-31 15:05:03 +0200

    Created the start of profile page.

---
+++ app/src/main/java/nl/hu_team/actortemplate/activity/ProfileActivity.java
@@ -0,0 +1,33 @@
+package nl.hu_team.actortemplate.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import nl.hu_team.actortemplate.R;
+import nl.hu_team.actortemplate.presenter.ProfileActivityPresenter;
+
+public class ProfileActivity extends AppCompatActivity implements ProfileActivityPresenter.ProfileActivityView{
+
+    private static final int GALlERY_INTENT = 2;
+    private ProfileActivityPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+        presenter = new ProfileActivityPresenter(this);
+    }
+
+    @OnClick(R.id.user_profile_photo) protected void changePhoto(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALlERY_INTENT);
+
+    }
+
+}
---
+++ app/src/main/java/nl/hu_team/actortemplate/presenter/ProfileActivityPresenter.java
@@ -0,0 +1,19 @@
+package nl.hu_team.actortemplate.presenter;
+
+import nl.hu_team.actortemplate.activity.ProfileActivity;
+
+public class ProfileActivityPresenter {
+
+    private final ProfileActivity view;
+
+    public ProfileActivityPresenter(ProfileActivity view) {
+        this.view = view;
+    }
+
+    public interface ProfileActivityView{
+
+    }
+
+
+
+}
